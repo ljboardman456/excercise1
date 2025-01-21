498b62fe1b3f8ecc0431b6cec2d7f5aca120faf7
levi boardman <leviboardman456@gmail.com>
2025-01-21 13:00:37 -0600

    commit

--- excercise1/Program.cs
+++ excercise1/Program.cs
@@ -10,6 +10,26 @@ namespace excercise1
     {
         static void Main(string[] args)
         {
+            //print prompt to ask for first name
+            Console.WriteLine("What is your first name?");
+            //collect input for first name
+            string firstName = Console.ReadLine();
+            //print prompt for last name
+            Console.WriteLine("What is your last name?");
+            //collect user input for last name
+            string lastName = Console.ReadLine();    
+            //print prompt for age
+            Console.WriteLine("What is your age?");
+            //collect input for age
+            string age = Console.ReadLine();    
+            //print prompt for color
+            Console.WriteLine("What is your color?");
+            //collect input for color
+            string color = Console.ReadLine();  
+            //print confirmed input info
+            Console.WriteLine("Hello, " + firstName + " " +lastName +"! You are " +age + " years old and your favorite color is " + color +".");
+            //wait for user input
+            Console.ReadLine();
         }
     }
 }
